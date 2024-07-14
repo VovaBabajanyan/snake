f79d8d1053c761479ebbeb51ead29dd62bd4a237
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-07-14 17:27:28 +0300

    no message

--- main.py
+++ main.py
@@ -47,6 +47,9 @@ def game_loop():
             message("Вы проиграли! Нажмите Q для выхода или C для повторной игры", red)
             pygame.display.update()
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    game_close = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         game_over = True
